0d39eebd597fdd05ef5337b3d6d29de6553d4d78
Arina <ob.arina21@gmail.com>
2026-04-19 21:55:18 +0400

    Added TestBase

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,9 +6,10 @@ import org.junit.jupiter.api.BeforeAll;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
-public class TextBoxTests {
+public class TestBase {
     @BeforeAll
     static void beforeAll() {
+        Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
     }
