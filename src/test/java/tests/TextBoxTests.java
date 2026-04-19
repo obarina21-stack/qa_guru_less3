66fbbdb5b5c67366b27d9d361cc61b69624e1b9c
Arina <ob.arina21@gmail.com>
2026-04-19 21:43:44 +0400

    Added selenide TextBoxTests

---
+++ src/test/java/tests/TextBoxTests.java
@@ -0,0 +1,36 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.files.DownloadActions.click;
+import static org.openqa.selenium.bidi.script.LocalValue.setValue;
+
+public class TextBoxTests {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void successfulFillFormTest() {
+        open("/text-box");
+        $("[id=userName]").setValue("Ivan Ivanov");
+        $("[id=userEmail]").setValue("ivanov@mail.com");
+        $("[id=currentAddress]").setValue("first address 1");
+        $("[id=permanentAddress]").setValue("second address 2");
+        $("[id=submit]").click();
+
+        $("[id=output] [id=name]").shouldHave(text("Ivan Ivanov"));
+        $("[id=output] [id=email]").shouldHave(text("ivanov@mail.com"));
+        $("[id=output] [id=currentAddress]").shouldHave(text("first address 1"));
+        $("[id=output] [id=permanentAddress]").shouldHave(text("second address 2"));
+    }
+}
